9711e78a85e350540d95781ea0dca97c860b4cbc
youssef <youssefboulaich33@gmail.com>
2023-08-21 09:54:38 +0200

    delete content

--- src/views/delete.view.php
+++
@@ -1,8 +0,0 @@
-<h1>Delete Hike</h1>
-
-<p>Are you sure you want to delete this hike : <?= $data['Hikes_Name'] ?> ?</p>
-
-<form action="#" method="post">
-    <input type="hidden" name="Hikes_Id" value="<?= $data['Hikes_Id'] ?>">
-    <button type="submit">Delete Hike</button>
-</form>
