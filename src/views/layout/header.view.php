7d631ca58197285ff0e303721830a7ffe3b19e93
J0K3RY <jokery03@hotmail.com>
2023-08-22 15:48:32 +0200

    css edit page + form

--- src/views/layout/header.view.php
+++ src/views/layout/header.view.php
@@ -5,16 +5,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.classless.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Workshop Classic Models</title>
 </head>
 <body>
     <header>
     <nav>
             <ul>
-                <li><a href="/"><strong>HikeQuest</strong></a></li>
+                <li class="title_header"><a href="/"><strong>HikeQuest</strong></a></li>
             </ul>
-            <ul>
+            <ul class="container_login_reg_header">
                 <?php if (!empty($_SESSION['user'])): ?>
                     Bonjour <?= $_SESSION['user']['username'] ?>
                     <li><a href="/logout">Logout</a></li>
