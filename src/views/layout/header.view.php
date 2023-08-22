284578f303e99e6fc74d2c802bcda895e21ec7f2
Pietro Scaduto <pietroscaduto@MacBook-Air-de-Pietro.local>
2023-08-22 15:49:54 +0200

    Add style index page

--- src/views/layout/header.view.php
+++ src/views/layout/header.view.php
@@ -5,8 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="scss/style.css">
-    <title>Workshop Classic Models</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>HikeQuest</title>
 </head>
 <body>
     <header>
@@ -16,10 +16,9 @@
             </ul>
             <ul>
                 <?php if (!empty($_SESSION['user'])): ?>
-                    Bonjour <?= $_SESSION['user']['username'] ?>
+                    <p><a href="/profil">Bonjour <?= $_SESSION['user']['username'] ?></a></p>
                     <li><a href="/logout">Logout</a></li>
-                    <li><a href="/profil">Profil</a></li>
-                    <li><a href="/Add">Add new Hike</a></li>
+                    <li class="blueButton"><a href="/Add">Add new Hike</a></li>
                 <?php else: ?>
                     <li><a href="/login">Login</a></li>
                     <li><a href="/register">Register</a></li>
